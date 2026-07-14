6c29981c79a6858eeaaea520eb5df0d75b6d1af3
Hamitha19 <hamithakumar19012006@gmail.com>
2026-07-14 14:01:58 +0530

    Create 3321-TypeOfTriangle

---
+++ 3321-TypeOfTriangle/3321-TypeOfTriangle.java
@@ -0,0 +1,16 @@
+// Last updated: 7/14/2026, 2:04:31 PM
+class Solution {
+    public String triangleType(int[] nums) {
+        if(nums[0]+nums[1]<=nums[2] || nums[1]+nums[2]<=nums[0] || nums[0]+nums[2]<=nums[1]){
+            return "none";
+        }
+        if(nums[0]==nums[1] && nums[1]==nums[2] ){
+            return "equilateral";
+        }else if(nums[0]==nums[1] || nums[1]==nums[2] || nums[2]==nums[0] ){
+            return "isosceles";
+        }
+            return "scalene";
+        
+        
+    }
+}
