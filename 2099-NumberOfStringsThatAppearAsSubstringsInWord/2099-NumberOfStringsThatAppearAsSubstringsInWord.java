2b0a95dd9a150acaf66b9f477d73146dfadad898
Hamitha19 <hamithakumar19012006@gmail.com>
2026-07-14 14:02:47 +0530

    Create 2099-NumberOfStringsThatAppearAsSubstringsInWord

---
+++ 2099-NumberOfStringsThatAppearAsSubstringsInWord/2099-NumberOfStringsThatAppearAsSubstringsInWord.java
@@ -0,0 +1,12 @@
+// Last updated: 7/14/2026, 2:05:21 PM
+class Solution {
+    public int numOfStrings(String[] patterns, String word) {
+        int count=0;
+        for(int i=0;i<patterns.length;i++){
+            if(word.contains(patterns[i])){
+                count++;
+            }
+        }
+        return count;
+    }
+}
