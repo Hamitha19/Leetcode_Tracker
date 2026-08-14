844a5df14ce7a84a73ecac6a2b577b64f5bbfaf9
Hamitha19 <hamithakumar19012006@gmail.com>
2026-08-14 16:13:26 +0530

    Create 1351-CountNegativeNumbersinaSortedMatrix

---
+++ 1351-CountNegativeNumbersinaSortedMatrix/version/java/1351-CountNegativeNumbersinaSortedMatrix_20260814_161341.java
@@ -0,0 +1,18 @@
+// Last updated: 8/14/2026, 4:13:41 PM
+1class Solution {
+2    public int findLucky(int[] arr) {
+3        int ans=-1;
+4        for(int i=0;i<arr.length;i++){
+5            int count=0;
+6            for(int j=0;j<arr.length;j++){
+7                if(arr[i]==arr[j]){
+8                    count++;
+9                }
+10            }
+11            if(count==arr[i]){
+12                ans=Math.max(ans,arr[i]);
+13            }
+14        }
+15        return ans;
+16    }
+17}
